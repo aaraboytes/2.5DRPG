0b29cba975b65037e618758a85f7ea97c52660f8
Andres Aboytes <chetobros@gmail.com>
2019-02-12 22:21:24 -0600

    Some changes

---
+++ Assets/LightModifier.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightModifier : MonoBehaviour
+{
+    public Color initialColor;
+    public Color endColor;
+    Light light;
+    private void Start()
+    {
+        light = GetComponent<Light>();
+        
+    }
+    private void Update()
+    {
+        Color currentColor = light.color;
+        light.color = Color.Lerp(currentColor, endColor, 0.01f);
+    }
+}
